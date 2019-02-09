def5182936da4e46f9339f171fc712dfade06b5e
Junior Marques <gilmar_junior@hotmail.com.br>
2019-02-09 21:45:36 +0000

    adjusts to slider and add new banner to project

--- js/section-slider.js
+++ js/section-slider.js
@@ -1,8 +1,8 @@
 $('.single-item-rtl').slick({
     rtl: true,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 2000
 });
 
-document.querySelector(".slick-prev").innerHTML = '<i class="fas fa-arrow-circle-left"></i>';
-document.querySelector(".slick-next").innerHTML = '<i class="fas fa-arrow-circle-right"></i>';
+document.querySelector(".slick-prev").innerHTML = '<i class="fas fa-caret-left"></i>';
+document.querySelector(".slick-next").innerHTML = '<i class="fas fa-caret-right"></i>';
